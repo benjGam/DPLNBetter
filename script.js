2d2492e20a0ef1642605f5d0e4f558b3c5bf0c39
Mecopi <mecopilive@gmail.com>
2022-10-08 15:41:01 +0200

    Adding custom Theme and custom Font color

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-let theme_color = "" //Let it blank if you don't want to add color theme else write hexa formatted color : #FFFFFF
-let text_color = "" //Let it blank to get default color text
+let theme_color = "#181818"; //Let it blank if you don't want to add color theme else write hexa formatted color : #FFFFFF
+let font_color = "#E7E7E7"; //Let it blank to get default color text else write hexa formatted color : #FFFFFF
+let font_balises = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong', 'ul']; //Do not modify
 
-function turn_on_personnalized_theme() {
-
+function apply_theme() {
+    document.getElementsByTagName('table')[1].setAttribute('style', 'width:100%;background-color:' + theme_color + ';');
+}
+function apply_font_color() {
+    font_balises.forEach(x => {
+        modify_font_color(x);
+    });
+    for (let x of document.getElementsByClassName('paragraph')) {
+        x.setAttribute('style', x.getAttribute('style') != null ? x.getAttribute('style') + 'color:' + font_color + ';' : 'color:' + font_color + ';');
+    }
+    for (let x of document.getElementsByClassName('fast-travel-coord')) {
+        x.setAttribute('style', x.getAttribute('style') != null ? x.getAttribute('style') + 'color:' + font_color + ';' : 'color:' + font_color + ';');
+    }
+}
+function modify_font_color(what) {
+    for (let x of document.getElementsByTagName(what)) {
+        x.setAttribute('style', x.getAttribute('style') != null ? x.getAttribute('style') + 'color:' + font_color + ';' : 'color:' + font_color + ';');
+    }
 }
 
 (function() {
-    if(document.URL.includes("https://www.dofuspourlesnoobs.com/")) //We're on website
-    {
+    if(document.URL.includes("https://www.dofuspourlesnoobs.com/")) //We're on DPLN
+    {   
         if(theme_color != '') {
-            turn_on_personnalized_theme()
+            apply_theme();
+        }
+        if(font_color != '') {
+            apply_font_color();
         }
     }
 
